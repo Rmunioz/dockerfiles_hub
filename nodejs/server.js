760b3a8157afd8ee34d95868a0e2a4a33c2e24bc
Rmunioz <81926716+Rmunioz@users.noreply.github.com>
2024-10-11 11:46:40 -0600

    se agrega server.js

--- nodejs/server.js
+++ nodejs/server.js
@@ -9,8 +9,24 @@ const HOST = config.HOST;
 // App
 const app = express();
 app.get('/', (req, res)=> {
-  res.send ('Welcome to the beginning of the todo app!!');
+  res.send ('Welcome to the beginning of the todo app *! ');
 });
+
+if (app.settings.env === "production") {
+  db.sequelize.sync().then(() => {
+      app.listen(PORT, () => {
+          console.log("App listening on PORT " + PORT);
+      });
+  });
+} else {
+  db.sequelize.sync({
+      force: true
+  }).then(() => {
+      app.listen(PORT, () => {
+          console.log("App listening on PORT " + PORT);
+      });
+  });
+}
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
