a56423e16b9deab5b2fca4ef0da3f29a4b304b74
System Administrator <root@MacBook-Air-de-Rosa.local>
2024-10-12 00:18:55 -0600

    version final

--- nodejs/config.js
+++ nodejs/config.js
@@ -1,5 +1,4 @@
 module.exports = {
     NODE_ENV: process.env.NODE_ENV || 'development',
-    HOST: process.env.HOST || '0.0.0.0',
-    PORT: process.env.PORT || 8000
+    HOST: process.env.HOST || '127.0.0.1'
   }
